1f61ab55fa75170e64f090f7152772a5fc39fe2c
kirithivasan08 <kirithivasan08@gmail.com>
2026-04-03 14:37:06 +0530

    Update nodemailer config to use port 587

--- utils/emailService.js
+++ utils/emailService.js
@@ -3,7 +3,9 @@ const nodemailer = require('nodemailer');
 // Create a transporter
 // For Gmail: use 'service': 'gmail'
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports (like 587 STARTTLS)
     auth: {
         user: process.env.EMAIL_USER,
         pass: (process.env.EMAIL_PASS || '').replace(/\s+/g, '') // Remove spaces if copied directly
